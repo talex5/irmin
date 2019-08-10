80798016006ee2e223cef5b12f54f4d4518bb274
Thomas Leonard <thomas.leonard@docker.com>
2019-08-10 11:18:23 +0100

    Disable Irmin's cohttp support
    CueKeeper doesn't need it and it causes opam conflicts.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -19,10 +19,9 @@ let opam =
 let () =
   Pkg.describe ~opams:[opam] "irmin" @@ fun c ->
   let git = Conf.value c git in
-  let http = Conf.value c http in
   let unix = Conf.value c unix in
   let mirage = Conf.value c mirage in
-  let tool = unix && http && git in
+  let tool = unix && false && git in
   let example x = Pkg.test ~cond:tool ~run:false ("examples/" ^ x) in
   Ok [
     Pkg.mllib ~api:["Irmin"] "lib/irmin.mllib";
@@ -30,7 +29,6 @@ let () =
     Pkg.mllib "lib/fs/irmin-fs.mllib";
     Pkg.mllib ~cond:git    "lib/git/irmin-git.mllib";
     Pkg.mllib ~cond:mirage "lib/mirage/irmin-mirage.mllib";
-    Pkg.mllib ~cond:http   "lib/http/irmin-http.mllib";
     Pkg.mllib ~cond:unix   "lib/unix/irmin-unix.mllib";
     Pkg.bin   ~cond:unix   "bin/main" ~dst:"irmin";
     Pkg.test  ~cond:tool ~dir:"lib_test" "lib_test/test" ~args:Cmd.(v "-e" % "-q");
